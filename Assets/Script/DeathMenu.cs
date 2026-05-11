a67355db5ed9614c29d17e454743d3b57fdd5542
DDvong <161529492+DDvong@users.noreply.github.com>
2026-05-11 15:24:47 -0700

    DeathMenu

---
+++ Assets/Script/DeathMenu.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour
+{
+    public GameObject deathPanel;
+
+    void Start()
+    {
+        deathPanel.SetActive(false);
+    }
+
+    public void ShowDeathScreen()
+    {
+        deathPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void TryAgain()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitToMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
